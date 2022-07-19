4cf0fde1922b1e9dc3e5338475f24a51eda45de7
pinkg-pig <2545589993@qq.com>
2022-07-19 12:53:53 +0800

    style:main布局

--- src/pages/svgenerate/Main.vue
+++ src/pages/svgenerate/Main.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class=" w-full h-full flex flex-col justify-center items-center dark:bg-[#222]">
+  <div class=" py-4 w-full h-full flex flex-col justify-between items-center dark:bg-[#222]">
+    <!-- name -->
+    <div> {{store.svgObj.name}} </div>
+
     <div>
-      <svg width="200" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <svg width="150" height="150" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <g transform="scale(5)" v-html="store.svgObj.body"></g>
       </svg>
     </div>
-
-    <!-- <div>
-      SVGenerate
-    </div> -->
+    <Footer></Footer>
   </div>
 </template>
 <script setup lang="ts">
