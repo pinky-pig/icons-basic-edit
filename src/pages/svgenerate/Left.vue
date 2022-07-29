4bff5e97909a7ada2143855ffd6ba43f8b1243ec
pinky-pig <2545589993@qq.com>
2022-07-29 11:56:11 +0800

    feat：列表使用虚拟列表

--- src/pages/svgenerate/Left.vue
+++ src/pages/svgenerate/Left.vue
@@ -1,47 +1,41 @@
 <template>
   <div class=" w-full h-full bg-[#FFFFFF] dark:bg-[#222] flex flex-col items-center">
-    <div class=" bg-[#f7f7f7] dark:bg-[#222222] dark:border-[#444444] w-full h-20 border border-r-0 border-[#e0e1e4] flex-center">
-      <input
-        id="input"
-        v-model="keyWord"
-        :placeholder="t('svgenerate.search-icon')"
-        :aria-label="t('svgenerate.search-icon')"
-        type="text"
-        autocomplete="false"
-        p="x4 y2"
-        w="80%"
-        h="60%"
-        text="center"
-        bg="#FFFFFF"
-        class=" dark:bg-[#282828]"
-        border="~ rounded gray-200 dark:gray-700"
-        outline="none active:none"
-        @keydown.enter="search"
-      >
+    <div
+      class=" bg-[#f7f7f7] dark:bg-[#222222] dark:border-[#444444] w-full h-20 border border-r-0 border-[#e0e1e4] flex-center">
+      <input id="input" v-model="keyWord" :placeholder="placeHolderText"
+        :aria-label="t('svgenerate.search-icon')" type="text" autocomplete="false" p="x4 y2" w="80%" h="60%"
+        text="center" bg="#FFFFFF" class=" dark:bg-[#282828]" border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none" @keydown.enter="search">
 
     </div>
 
-    <div class="flex-1 py-4 overflow-auto flex flex-wrap content-start " >
-      <div class=" w-30 h-30 text-center cursor-pointer hover:bg-[#f7f7f7] dark:hover:bg-[#282828]" @click="handleClick(item)"  v-for="(item) in iconList">
+    <div class="flex-1 py-4 overflow-auto flex flex-wrap content-start ">
 
-        <div style="font-size:30px" class=" w-[30px] h-[30px] mx-auto flex flex-row items-center justify-center mt-[25px] mb-[10px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            aria-hidden="true"
-            role="img"
-            width="1em"
-            height="1em"
-            preserveAspectRatio="xMidYMid meet"
-            :viewBox=viewBox
-            >
-            <g v-html="item.body" >
-            </g>
-          </svg>
+      <div v-bind="containerProps" style="height:100%" class="overflow-auto  rounded">
+        <div v-bind="wrapperProps">
+          <div v-for="{ index, data } in list" :key="index" :style="{
+            height: `${120}px`,
+            display: 'flex',
+            justifyContent: 'start',
+            alignItems: 'center',
+          }">
+            <div @click="handleClick(i)" class=" w-30 h-30 text-center cursor-pointer hover:bg-[#f7f7f7] dark:hover:bg-[#282828]"
+              v-for="i in data">
+              <div style="font-size:30px"
+                class=" w-[30px] h-[30px] mx-auto flex flex-row items-center justify-center mt-[25px] mb-[10px]">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
+                  role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" :viewBox=viewBox>
+                  <g v-html="i.body"> </g>
+                </svg>
+              </div>
+
+              <div class="w-[90px] mx-auto" style="font-size: 0.25rem;">{{ i.name }}</div>
+            </div>
+
+          </div>
         </div>
-
-        <div class="w-[90px] mx-auto" style="font-size: 0.25rem;">{{item.name}}</div>
       </div>
+
     </div>
 
 
@@ -49,45 +43,35 @@
 </template>
 <script setup lang="ts">
 import axios from 'axios';
-
-
-
-const { t } = useI18n()
-const keyWord = $ref('')
-const search = () => {
-  if (keyWord)
-    console.log("检索");
-}
 const store = useSvgenerateStore()
 const storeSvg = useSvgStore()
+/**
+ * 使用 vueuse 的虚拟列表 useVirtualList。
+ * 注意点：设置高度和itemHeight
+ */
 
 /** icon 的尺寸 */
 const viewBox = ref('0 0 32 32')
-const handleClick = (svgObj:any) => {
+const handleClick = (svgObj: any) => {
   let obj = {
-    name:svgObj.name,
-    body:svgObj.body,
-    width:svgObj.width,
-    height:svgObj.height,
-    property:{
-      stokeColor:'',
-      stokeWidth:0,
-      fillColor:''
+    name: svgObj.name,
+    body: svgObj.body,
+    width: svgObj.width,
+    height: svgObj.height,
+    property: {
+      stokeColor: '',
+      stokeWidth: 0,
+      fillColor: ''
     }
   }
   store.setCurrentSvg(obj)
 
   storeSvg.resetSvgStore()
 }
-onMounted(()=>{
-  fetchData()
-})
 const iconList = ref()
-const fetchData = () => {
-  axios.get('http://localhost:3200/form/fetchSvgFromIconify').then((res:any) => {
+const fetchData = async () => {
+  await axios.get('http://localhost:3200/form/fetchSvgFromIconify').then((res:any) => {
     let { icons,height,width } = res.data
-    viewBox.value = `0 0 ${width} ${height}`
-
     let keysArr = Object.keys(icons)
     let result = keysArr.map((i:string) => {
       return {
@@ -97,9 +81,41 @@ const fetchData = () => {
         body:icons[i].body
       }
     })
-    iconList.value = result.slice(0,10)
-    // console.log(result);
+    // iconList.value = result.slice(0,100)
+    iconList.value = result
+    const sliceBy3 = (arr:Array<any>,result:Array<any> = []) => {
+      if(arr.length == 0)
+        return
+      result.push(arr.splice(0,3))
+      sliceBy3(arr,result)
+      return result
+    }
+    iconList.value = sliceBy3(iconList.value)
   })
 }
+await fetchData()
+
+const { list, containerProps, wrapperProps } = useVirtualList(
+  iconList,
+  {
+    itemHeight: 120,
+  },
+)
+
+
+/**
+ * 检索
+ */
+const { t } = useI18n()
+const keyWord = $ref('')
+const placeHolderText = ref(t('svgenerate.search-icon') + ' (' + iconList.value.length + ')')
+const search = () => {
+  if (keyWord)
+    console.log("检索");
+}
+
+
+
+
 
 </script>
--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,37 +1,40 @@
 <script setup lang="ts">
+import axios from 'axios';
+const iconList = ref()
+const fetchData = async () => {
+  await axios.get('http://localhost:3200/form/fetchSvgFromIconify').then((res:any) => {
+    let { icons,height,width } = res.data
+    let keysArr = Object.keys(icons)
+    let result = keysArr.map((i:string) => {
+      return {
+        name:i,
+        width,
+        height,
+        body:icons[i].body
+      }
+    })
+    iconList.value = result.slice(0,100)
+    const sliceBy3 = (arr:Array<any>,result:Array<any> = []) => {
+      if(arr.length == 0)
+        return
+      result.push(arr.splice(0,3))
+      sliceBy3(arr,result)
+      return result
+    }
+    iconList.value = sliceBy3(iconList.value)
 
-let url = 'https://img1.baidu.com/it/u=1966616150,2146512490&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1659114000&t=08e0d8330904b0bbf831140c1978d4cd'
-let allItems = Array.from(Array(100).keys(), (item,index)=>{return {url,idx:item}});
+  })
+}
+await fetchData()
+
+// let url = 'https://img1.baidu.com/it/u=1966616150,2146512490&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1659114000&t=08e0d8330904b0bbf831140c1978d4cd'
+// let allItems = Array.from(Array(100).keys(), (item,index)=>{return {url,idx:item}});
 const { list, containerProps, wrapperProps } = useVirtualList(
-  allItems,
+  iconList,
   {
-    itemHeight: 22,
+    itemHeight: 120,
   },
 )
-
-
-
-// const iconList = ref()
-// const fetchData = () => {
-//   axios.get('http://localhost:3200/form/fetchSvgFromIconify').then((res:any) => {
-//     let { icons,height,width } = res.data
-//     viewBox.value = `0 0 ${width} ${height}`
-
-//     let keysArr = Object.keys(icons)
-//     let result = keysArr.map((i:string) => {
-//       return {
-//         name:i,
-//         width,
-//         height,
-//         body:icons[i].body
-//       }
-//     })
-//     iconList.value = result.slice(0,10)
-//     // console.log(result);
-//   })
-// }
-
-
 
 
 </script>
@@ -39,21 +42,41 @@ const { list, containerProps, wrapperProps } = useVirtualList(
 <template>
   <div>
 
-    <div v-bind="containerProps" style="height:300px" class="overflow-auto p-2 bg-gray-500/5 rounded">
+    <div v-bind="containerProps" style="height:400px" class="overflow-auto p-2 bg-gray-500/5 rounded">
       <div v-bind="wrapperProps">
         <div
           v-for="{ index, data } in list"
           :key="index"
           class="border border-$c-divider mb-2"
           :style="{
-            height: `${42}px`,
+            height: `${120}px`,
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
           }"
         >
-          <img class=" w-20px h-30px" :src="data.url" alt="">
-          Row {{ index }}
+
+          <div class=" w-30 h-30 text-center cursor-pointer hover:bg-[#f7f7f7] dark:hover:bg-[#282828]" v-for="i in data">
+            <div style="font-size:30px" class=" w-[30px] h-[30px] mx-auto flex flex-row items-center justify-center mt-[25px] mb-[10px]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                aria-hidden="true"
+                role="img"
+                width="1em"
+                height="1em"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox='0 0 32 32'
+                >
+                <g v-html="i.body" > </g>
+              </svg>
+            </div>
+
+            <div class="w-[90px] mx-auto" style="font-size: 0.25rem;">{{i.name}}</div>
+          </div>
+
+
+
         </div>
       </div>
     </div>
