713546c9fcf281abf55b1503940367ffd5e88842
pinky-pig <2545589993@qq.com>
2022-08-16 19:05:34 +0800

    feat

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,17 +1,27 @@
 <template>
-  <svg width="100%" height="100%">
-    <line 
-      :x1="props.viewPortX" 
-      y1="0" 
-      :x2="props.viewPortX + props.viewPortWidth" 
-      y2="0" 
-      stroke="orange" 
-      fill="transparent" 
-      stroke-width="5"
+  <svg width="100%" height="100%" >
+    <line
+      :x1="props.viewPortX"
+      y1="0"
+      :x2="props.viewPortX + props.viewPortWidth"
+      y2="0"
+      stroke="orange"
+      fill="transparent"
+      stroke-width="0.08241758241758242"
+      />
+    <line
+      :x1="0"
+      :y1="props.viewPortY"
+      :x2="0"
+      :y2="props.viewPortY + props.viewPortHeight"
+      stroke="orange"
+      fill="transparent"
+      stroke-width="0.08241758241758242"
       />
   </svg>
 </template>
 <script setup lang="ts">
+
 const props = defineProps({
   viewPortX: {
     type: Number,
@@ -23,11 +33,30 @@ const props = defineProps({
   },
   viewPortWidth: {
     type: Number,
-    default: 50
+    default: 0
   },
   viewPortHeight: {
     type: Number,
     default: 0
   }
 })
+
+let width = computed(()=> props.viewPortWidth)
+// let height = computed(()=> props.viewPortHeight)
+
+watch(width,()=>{
+  let xGrid = Array(Math.ceil(width.value) + 1).fill(null).map((_, i) => Math.floor(width.value) + i);
+  // console.log(yGrid);
+  // let yGrid = Array(Math.ceil(height.value) + 1).fill(null).map((_, i) => Math.floor(height.value) + i);
+  console.log(xGrid);
+})
+
+// watch(height,()=>{
+//   let xGrid = Array(Math.ceil(width.value) + 1).fill(null).map((_, i) => Math.floor(width.value) + i);
+//   let yGrid = Array(Math.ceil(height.value) + 1).fill(null).map((_, i) => Math.floor(height.value) + i);
+//   console.log(xGrid);
+//   console.log(yGrid);
+// })
+
+
 </script>
--- src/pages/editor/index.vue
+++ src/pages/editor/index.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="svg-editor-container ">
-    <Canvas></Canvas>
+  <div class="flex flex-row w-screen h-screen ">
+    <div class="w-500px h-full">
+
+    </div>
+    <div ref="canvas" class=" flex-1">
+      <Canvas :viewPortWidth="width" :viewPortHeight="height"></Canvas>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 
+const canvas = ref(null)
+const { width, height } = useElementBounding(canvas)
+
 </script>
+
+<route lang="yaml">
+name: icons-editor
+path: /icons-editor
+meta:
+  layout: home
+  requiresAuth: false
+  id: 2
+  string: "2"
+</route>
