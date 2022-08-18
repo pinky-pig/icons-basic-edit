6ab8c22023025d943cb2adbdfd5c3e2203d2ce27
pinky-pig <2545589993@qq.com>
2022-08-18 18:42:26 +0800

    feat: canvas画布修改

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -9,7 +9,7 @@
       y1="0"
       :x2="props.viewPortX + props.viewPortWidth"
       y2="0"
-      stroke="orange"
+      stroke="currentColor"
       fill="transparent"
       :stroke-width="strokeWidth * 4"
       />
@@ -18,7 +18,7 @@
       :y1="props.viewPortY"
       :x2="0"
       :y2="props.viewPortY + props.viewPortHeight"
-      stroke="orange"
+      stroke="currentColor"
       fill="transparent"
       :stroke-width="strokeWidth * 4"
       />
@@ -42,6 +42,14 @@
       fill="transparent"
       :stroke-width="strokeWidth"
       />
+
+    <path
+      class="filled"
+      :stroke-width="strokeWidth"
+      stroke="currentColor"
+      fill="transparent"
+      :d="rawPath"
+    />
   </svg>
 </template>
 <script setup lang="ts">
@@ -66,18 +74,44 @@ const props = defineProps({
   strokeWidth: {
     type: Number,
     default: 1
-  }
-})
+  },
 
-const width = computed(()=> props.viewPortWidth)
-const height = computed(()=> props.viewPortHeight)
+  canvasWidth: {
+    type: Number,
+    default: 100
+  },
+  canvasHeight: {
+    type: Number,
+    default: 100
+  },
+
+  rawPath: {
+    type: String,
+    default: ''
+  },
+})
 
 const xGrid = ref()
 const yGrid = ref()
-watch([width,height],()=>{
-  xGrid.value = Array(Math.ceil(width.value) + 1).fill(null).map((_, i) => Math.floor(props.viewPortX) + i);
-  yGrid.value = Array(Math.ceil(height.value) + 1).fill(null).map((_, i) => Math.floor(props.viewPortY) + i);
+watch(props,()=>{
+  if (5 * props.viewPortWidth <= props.canvasWidth) {
+    xGrid.value = Array(Math.ceil(props.viewPortWidth) + 1).fill(null).map((_, i) => Math.floor(props.viewPortX) + i)
+    yGrid.value = Array(Math.ceil(props.viewPortHeight) + 1).fill(null).map((_, i) => Math.floor(props.viewPortY) + i)
+  } else {
+    xGrid.value = []
+    yGrid.value = []
+  }
 },{ immediate:true })
 
 
+// M = moveto(M X,Y) ：将画笔移动到指定的坐标位置
+// L = lineto(L X,Y) ：画直线到指定的坐标位置
+// H = horizontal lineto(H X)：画水平线到指定的X坐标位置
+// V = vertical lineto(V Y)：画垂直线到指定的Y坐标位置
+// C = curveto(C X1,Y1,X2,Y2,ENDX,ENDY)：三次贝赛曲线
+// S = smooth curveto(S X2,Y2,ENDX,ENDY)
+// Q = quadratic Belzier curve(Q X,Y,ENDX,ENDY)：二次贝赛曲线
+// T = smooth quadratic Belzier curveto(T ENDX,ENDY)：映射
+// A = elliptical Arc(A RX,RY,XROTATION,FLAG1,FLAG2,X,Y)：弧线
+// Z = closepath()：关闭路径
 </script>
--- src/pages/editor/index.vue
+++ src/pages/editor/index.vue
@@ -1,53 +1,67 @@
 <template>
-  <div class="flex flex-row w-screen h-screen ">
-    <div ref="element" class="w-500px h-full">
+  <div class="flex flex-row w-screen h-screen bg-[#f7f7f7] dark:bg-[#222222] text-[#374151] dark:text-[#e5e7eb] ">
 
-    </div>
-    <div ref="canvas" class=" flex-1 min-w-10">
+    <div ref="canvas" class=" w-full h-full min-w-10">
       <Canvas
         :viewPortWidth="cfg.viewPortWidth"
         :viewPortHeight="cfg.viewPortHeight"
         :viewPortX="cfg.viewPortX"
         :viewPortY="cfg.viewPortY"
         :strokeWidth="strokeWidth"
+        :canvasWidth="canvasWidth"
+        :canvasHeight="canvasHeight"
+        :rawPath="kDefaultPath"
         ></Canvas>
     </div>
+
+    <Footer class=" absolute left-[calc(50%-80px)] bottom-0"></Footer>
   </div>
 </template>
 <script lang="ts" setup>
 
-let canvasWidth = 100
-let canvasHeight = 100
+const canvasWidth = ref(100)
+const canvasHeight = ref(100)
 let cfg = reactive({
-  viewPortX:0,
-  viewPortY:0,
-  viewPortWidth:0,
-  viewPortHeight:0,
+  viewPortX: 0,
+  viewPortY: 0,
+  viewPortWidth: 0,
+  viewPortHeight: 0,
 })
 let strokeWidth = ref(0)
 
 const canvas = ref(null)
 const { width, height } = useElementBounding(canvas)
-watch([width,height],() => {
-  canvasWidth = width.value
-  canvasHeight = height.value
-  zoomAuto()
+watch([width, height], () => {
+  canvasWidth.value = width.value
+  canvasHeight.value = height.value
+
+  updateViewPort(
+    cfg.viewPortX,
+    cfg.viewPortY,
+    cfg.viewPortWidth,
+    null,
+    true
+  )
 })
 
-// useEventListener(canvas, 'wheel', (evt:WheelEvent) => {
-//   const scale = Math.pow(1.005, evt.deltaY);
-//   const pt = eventToLocation(evt);
-//   zoomViewPort(scale, pt);
-// })
-zoomAuto()
-
 /**
- * 滚轮放大缩小更新计算视图 ViewPort
+ * 初始化画布
  */
-function zoomAuto(){
-  const rawPath = "M 4 8 L 10 1 L 13 0 L 12 3 L 5 9 C 6 10 6 11 7 10 C 7 11 8 12 7 12 A 1.42 1.42 0 0 1 6 13 A 5 5 0 0 0 4 10 Q 3.5 9.9 3.5 10.5 T 2 11.8 T 1.2 11 T 2.5 9.5 T 3 9 A 5 5 90 0 0 0 7 A 1.42 1.42 0 0 1 1 6 C 1 5 2 6 3 6 C 2 7 3 7 4 8 M 10 1 L 10 3 L 12 3 L 10.2 2.8 L 10 1"
+onMounted(() => {
+  setTimeout(() => {
+    zoomAuto();
+  }, 0);
+})
+// 初始化默认的icon
+const kDefaultPath = ref(
+  `M 4 8 L 10 1 L 13 0 L 12 3 L 5 9 C 6 10 6 11 7 10 C 7 11 8 12 7 12 A 1.42 1.42 0 0 1 6 13 `
++ `A 5 5 0 0 0 4 10 Q 3.5 9.9 3.5 10.5 T 2 11.8 T 1.2 11 T 2.5 9.5 T 3 9 A 5 5 90 0 0 0 7 A 1.42 1.42 0 0 1 1 6 `
++ `C 1 5 2 6 3 6 C 2 7 3 7 4 8 M 10 1 L 10 3 L 12 3 L 10.2 2.8 L 10 1`
+)
+function zoomAuto() {
+  const rawPath = kDefaultPath.value
   const bbox = browserComputePathBoundingBox(rawPath);
-  const k = canvasHeight / canvasWidth;
+  const k = canvasHeight.value / canvasWidth.value;
   let w = bbox.width + 2;
   let h = bbox.height + 2;
   if (k < h / w) {
@@ -92,11 +106,11 @@ function browserComputePathBoundingBox(path) {
  * @param force 是否锁定
  */
 function updateViewPort(x: number, y: number, w: number | null, h: number | null, force = false) {
-  if (w === null && h !==null) {
-    w = canvasWidth * h / canvasHeight;
+  if (w === null && h !== null) {
+    w = canvasWidth.value * h / canvasHeight.value;
   }
-  if (h === null && w !==null) {
-    h = canvasHeight * w / canvasWidth;
+  if (h === null && w !== null) {
+    h = canvasHeight.value * w / canvasWidth.value;
   }
   if (!w || !h) {
     return;
@@ -105,33 +119,106 @@ function updateViewPort(x: number, y: number, w: number | null, h: number | null
   cfg.viewPortY = parseFloat((1 * y).toPrecision(6));
   cfg.viewPortWidth = parseFloat((1 * w).toPrecision(4));
   cfg.viewPortHeight = parseFloat((1 * h).toPrecision(4));
-  strokeWidth.value = cfg.viewPortWidth / canvasWidth;
+  strokeWidth.value = cfg.viewPortWidth / canvasWidth.value;
 }
 
+
+
 /**
- * wheel滚动的位置
- * @param event wheel 事件
- * @param idx
+ * 滚轮放大缩小更新计算视图 ViewPort
  */
-// function eventToLocation(event: MouseEvent | TouchEvent, idx = 0): {x: number, y: number} {
-//   const { top, left,} = useElementBounding(canvas)
-//   const touch = event instanceof MouseEvent ? event : event.touches[idx];
-//   const x = cfg.viewPortX + (touch.clientX - left.value) * strokeWidth.value;
-//   const y = cfg.viewPortY + (touch.clientY - top.value) * strokeWidth.value;
-//   return {x, y};
-// }
+useEventListener(canvas, 'wheel', (evt: WheelEvent) => {
+  const scale = Math.pow(1.005, evt.deltaY);
+  const pt = eventToLocation(evt);
+
+  zoomViewPort(scale, pt);
+})
+
 /**
  * 更新viewport
  */
-// function zoomViewPort(scale: number,  pt?: {x: number, y: number}){
-//   if (!pt) {
-//     pt = {x: cfg.viewPortX + 0.5 * cfg.viewPortWidth, y: cfg.viewPortY + 0.5 * cfg.viewPortHeight};
-//   }
-//   cfg.viewPortWidth = scale * cfg.viewPortWidth;
-//   cfg.viewPortHeight = scale * cfg.viewPortHeight;
-//   cfg.viewPortX = cfg.viewPortX + ((pt.x - cfg.viewPortX) - scale * (pt.x - cfg.viewPortX));
-//   cfg.viewPortY = cfg.viewPortY + ((pt.y - cfg.viewPortY) - scale * (pt.y - cfg.viewPortY));
-// }
+function zoomViewPort(scale: number, pt?: { x: number, y: number }) {
+  if (!pt) {
+    pt = { x: cfg.viewPortX + 0.5 * cfg.viewPortWidth, y: cfg.viewPortY + 0.5 * cfg.viewPortHeight };
+  }
+  let x = cfg.viewPortX + ((pt.x - cfg.viewPortX) - scale * (pt.x - cfg.viewPortX));
+  let y = cfg.viewPortY + ((pt.y - cfg.viewPortY) - scale * (pt.y - cfg.viewPortY));
+  let w = scale * cfg.viewPortWidth;
+  let h = scale * cfg.viewPortHeight;
+  updateViewPort( x, y, w, h )
+}
+
+/** 拖拽移动画布 canvas */
+let draggedEvt: any = null
+useEventListener(canvas, 'mousedown', (evt: MouseEvent) => {
+  draggedEvt = evt
+})
+useEventListener(canvas, 'mouseup', (evt: MouseEvent) => {
+  stopDrag()
+})
+useEventListener(canvas, 'mousemove', (evt: MouseEvent) => {
+  drag(evt)
+})
+
+function eventToLocation(event: MouseEvent | TouchEvent, idx = 0): { x: number, y: number } {
+  const { top, left, } = useElementBounding(canvas)
+
+  const touch = event instanceof MouseEvent ? event : event.touches[idx];
+  const x = cfg.viewPortX + (touch.clientX - left.value) * strokeWidth.value;
+  const y = cfg.viewPortY + (touch.clientY - top.value) * strokeWidth.value;
+
+  return { x, y };
+}
+
+function drag(event: MouseEvent | TouchEvent) {
+  if (draggedEvt) {
+
+    const pt = eventToLocation(event);
+    const pinchToZoomValue = pinchToZoom(draggedEvt, event)
+    if (pinchToZoomValue !== null){
+      const w = pinchToZoomValue.zoom * cfg.viewPortWidth;
+      const h = pinchToZoomValue.zoom * cfg.viewPortHeight;
+      const x = cfg.viewPortX + pinchToZoomValue.delta.x + (pinchToZoomValue.center.x - cfg.viewPortX) * (1 - pinchToZoomValue.zoom);
+      const y = cfg.viewPortY + pinchToZoomValue.delta.y + (pinchToZoomValue.center.y - cfg.viewPortY) * (1 - pinchToZoomValue.zoom);
+
+      updateViewPort( x, y, w, h )
+    } else {
+      const oriPt = eventToLocation(draggedEvt);
+      let x = cfg.viewPortX + (oriPt.x - pt.x)
+      let y = cfg.viewPortY + (oriPt.y - pt.y)
+      let w = cfg.viewPortWidth
+      let h = cfg.viewPortHeight
+
+      updateViewPort( x, y, w, h )
+    }
+    draggedEvt = event;
+  }
+}
+function pinchToZoom(previousEvent: MouseEvent | TouchEvent, event: MouseEvent | TouchEvent){
+  if (window.TouchEvent
+    && previousEvent instanceof TouchEvent
+    && event instanceof TouchEvent
+    && previousEvent.touches.length >= 2
+    && event.touches.length >= 2) {
+    const pt = eventToLocation(event, 0);
+    const pt2 = eventToLocation(event, 1);
+    const oriPt = eventToLocation(previousEvent, 0);
+    const oriPt2 = eventToLocation(previousEvent, 1);
+    const ptm = { x: 0.5 * (pt.x + pt2.x), y: 0.5 * (pt.y + pt2.y) };
+    const oriPtm = { x: 0.5 * (oriPt.x + oriPt2.x), y: 0.5 * (oriPt.y + oriPt2.y) };
+    const delta = { x: oriPtm.x - ptm.x, y: oriPtm.y - ptm.y };
+    const k =
+      Math.sqrt((oriPt.x - oriPt2.x) * (oriPt.x - oriPt2.x) + (oriPt.y - oriPt2.y) * (oriPt.y - oriPt2.y)) /
+      Math.sqrt((pt.x - pt2.x) * (pt.x - pt2.x) + (pt.y - pt2.y) * (pt.y - pt2.y));
+    return { zoom: k, delta, center: ptm };
+  }
+  return null;
+}
+
+function stopDrag() {
+  draggedEvt = null;
+}
+
 </script>
 
 <route lang="yaml">
