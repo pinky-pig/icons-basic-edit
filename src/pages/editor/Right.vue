c24f470c2ad0e7cd3107839e6630bf4563dc2e2c
pinky-pig <2545589993@qq.com>
2022-08-29 19:05:19 +0800

    docs

--- src/pages/editor/Right.vue
+++ src/pages/editor/Right.vue
@@ -7,7 +7,7 @@
   <div class="flex flex-col fixed right-30px top-120px w-[24.5rem] h-[75vh] rounded-2xl overflow-hidden ">
     <div class="absolute w-20 h-1 top-2 rounded-md  bg-[var(--property-header-color)] z-10" style="left: calc(50% - 2.5rem);"></div>
     <div class="border-b border-slate-400/20 w-full h-[80px] mt-1 p-4 text-3xl flex items-center rounded-t z-0 bg-[var(--property-bg-color)] text-[var(--property-text-color)] ">
-      propertys
+      Propertys
     </div>
 
     <div class="flex-1 w-full py-4 overflow-auto rounded-b-2xl divide-y divide-slate-400/20 bg-[var(--property-bg-color)] text-[var(--property-text-color)] ">
