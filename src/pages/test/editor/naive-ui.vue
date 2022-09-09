babc90d41ade82ebc9d42c1116c72249955e99e1
pinky-pig <2545589993@qq.com>
2022-09-09 11:55:04 +0800

    refactor: 清理代码结构

---
+++ src/pages/test/editor/naive-ui.vue
@@ -0,0 +1,23 @@
+<template>
+  <n-loading-bar-provider>
+    <n-dialog-provider>
+      <n-notification-provider>
+        <n-message-provider>
+          <slot></slot>
+          <naive-provider-content />
+        </n-message-provider>
+      </n-notification-provider>
+    </n-dialog-provider>
+  </n-loading-bar-provider>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, h } from 'vue'
+const NaiveProviderContent = defineComponent({
+  setup() { },
+  render() {
+    return h('div')
+  },
+})
+</script>
+<style scoped></style>
