0fae1f5ee11ff0206b59736c1fbfa9d44e8314bf
pinky-pig <2545589993@qq.com>
2022-09-09 17:32:27 +0800

    fixed: 修改 Transform dom 结构

--- src/pages/edit/PathPropertyPanel.vue
+++ src/pages/edit/PathPropertyPanel.vue
@@ -5,13 +5,16 @@ const props = useSvgPathStore()
 
 initMatrix(props)
 
-// const transformType = [
-//   { label:'Scale', value:[ {x:1}, {y:1}, ] },
-//   { label:'Translate', value:[ {x:1}, {y:1}, ] },
-//   { label:'Skew', value:[ {x:1}, {y:1}, ] },
-//   { label:'Rotate', value:[ {'角度':1}, ] },
-//   { label:'Transform-origin', value:[ {'center':1}, ] },
-// ]
+const transformType = [
+  { label:'Scale', value:[ props.scaleX, props.scaleY ] },
+  { label:'Translate', value:[ props.translateX, props.translateY, ] },
+  // { label:'Skew', value:[ {x:1}, {y:1}, ] },
+  // { label:'Rotate', value:[ {'角度':1}, ] },
+  // { label:'Transform-origin', value:[ {'center':1}, ] },
+]
+
+console.log(transformType);
+
 
 </script>
 
@@ -28,47 +31,18 @@ initMatrix(props)
         Transform
       </div>
 
-      <div class="h-34px overflow-auto cursor-default px-3 mx-2 flex flex-row justify-center items-center gap-2">
+      <div v-for="(item, index) in transformType" :key="index" class="h-34px overflow-auto cursor-default px-3 mx-2 flex flex-row justify-center items-center gap-2">
         <div class="text-lg flex justify-center items-center rounded-lg w-1/3 h-full bg-[var(--input-bg-color)] text-[var(--input-text-color)] overflow-hidden">
-          Scale
-        </div>
-        <div style="border-radius: 10px;" class=" x-input w-1/3 h-full pl-8 pr-4 bg-[var(--input-bg-color)] text-[var(--input-text-color)] overflow-hidden">
-          <input
-            type="text"
-            style="font-size:18px;outline: none;"
-            class=" w-full h-full border-0 focus:ring-0 bg-[var(--input-bg-color)] text-[var(--input-text-color)]"
-            v-model="props.scaleX"
-            />
-        </div>
-        <div style="border-radius: 10px;" class=" y-input w-1/3 h-full pl-8 pr-4 bg-[var(--input-bg-color)] text-[var(--input-text-color)] overflow-hidden">
-          <input
-            type="text"
-            style="font-size:18px;outline: none;"
-            class=" w-full h-full border-0 focus:ring-0 bg-[var(--input-bg-color)] text-[var(--input-text-color)]"
-            v-model="props.scaleY"
-            />
-        </div>
-      </div>
-
-      <div class="h-34px overflow-auto cursor-default px-3 mx-2 flex flex-row justify-center items-center gap-2">
-        <div class="text-lg flex justify-center items-center rounded-lg w-1/3 h-full bg-[var(--input-bg-color)] text-[var(--input-text-color)] overflow-hidden">
-          Translate
+          {{item.label}}
         </div>
 
-        <div style="border-radius: 10px;" class=" x-input w-1/3 h-full pl-8 pr-4 bg-[var(--input-bg-color)] text-[var(--input-text-color)] overflow-hidden">
+        <div v-for="it,idx in item.value" :key="it" style="border-radius: 10px;" class=" relative w-1/3 h-full pl-8 pr-4 bg-[var(--input-bg-color)] text-[var(--input-text-color)] overflow-hidden">
+          <div class="input-label">{{idx === 0 ? 'x' : 'y'}}</div>
           <input
             type="text"
             style="font-size:18px;outline: none;"
             class=" w-full h-full border-0 focus:ring-0 bg-[var(--input-bg-color)] text-[var(--input-text-color)]"
-            v-model="props.translateX"
-            />
-        </div>
-        <div style="border-radius: 10px;" class=" y-input w-1/3 h-full pl-8 pr-4 bg-[var(--input-bg-color)] text-[var(--input-text-color)] overflow-hidden">
-          <input
-            type="text"
-            style="font-size:18px;outline: none;"
-            class=" w-full h-full border-0 focus:ring-0 bg-[var(--input-bg-color)] text-[var(--input-text-color)]"
-            v-model="props.translateY"
+            v-model="transformType[index]['value'][idx]"
             />
         </div>
       </div>
@@ -78,26 +52,13 @@ initMatrix(props)
   </div>
 </template>
 <style>
-.x-input::before{
-  content: 'x';
+.input-label{
   color: var(--input-text-color);
   font-family: Inter;
   font-weight: 400;
   font-size: 14px;
-  margin-top: 5px;
-  margin-left: -20px;
-  text-align: right;
   position: absolute;
-}
-.y-input::before{
-  content: 'y';
-  color: var(--input-text-color);
-  font-family: Inter;
-  font-weight: 400;
-  font-size: 14px;
-  margin-top: 5px;
-  margin-left: -20px;
-  text-align: right;
-  position: absolute;
+  left: 10px;
+  top: 5px;
 }
 </style>
