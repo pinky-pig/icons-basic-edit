3eccbc3c5185a2058a0db641859134b796348441
pinky-pig <2545589993@qq.com>
2023-01-13 12:29:07 +0800

    fix: 解开gsap注释

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import edit from './edit/index.vue'
+import IconBasicEdit from './icon-basic-edit/index.vue'
 </script>
 
 <template>
   <edit />
+  <!-- <IconBasicEdit /> -->
 </template>
 
 <route lang="yaml">
