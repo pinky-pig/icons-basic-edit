fe964406ede00bbbf2c641e49869b6fa08874899
pinky-pig <2545589993@qq.com>
2023-06-25 17:53:18 +0800

    chore: init layout

--- src/pages/edit/index.vue
+++ src/pages/edit/index.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 import PathCanvas from './PathCanvas.vue'
 import PathToolsPanel from './PathToolsPanel.vue'
-import PathPropertyPanel from './PathPropertyPanel.vue'
-import PathAnimatePanel from './PathAnimatePanel.vue'
 </script>
 
 <template>
   <n-config-provider :theme="naiveUIDarkTheme" :theme-overrides="naiveUIThemeOverrides">
     <div
-      class="app-wrapper z-99 w-screen h-screen p-8 flex flex-col gap-8 bg-[var(--main-background-image)]"
+      class="app-wrapper z-99 w-screen h-screen p-4 flex flex-col gap-8 bg-[var(--main-background-image)]"
     >
-      <div class=" w-full h-2/3 rounded-3xl gap-8 flex flex-row justify-between">
+      <div class=" w-full h-full rounded-3xl gap-4 flex flex-row justify-between">
         <div class="w-350px h-full rounded-3xl bg-[var(--tool-bg-color)] text-[var(--tool-text-color)]">
           <PathToolsPanel />
         </div>
@@ -20,14 +18,6 @@ import PathAnimatePanel from './PathAnimatePanel.vue'
             style="background-image: var(--canvas-bg-imgae);"
           />
         </div>
-
-        <div class="w-350px h-full rounded-3xl bg-[var(--tool-bg-color)] text-[var(--tool-text-color)] ">
-          <PathPropertyPanel />
-        </div>
-      </div>
-
-      <div class=" w-full flex-1 rounded-3xl bg-[var(--animate-bg-color)] overflow-hidden">
-        <PathAnimatePanel />
       </div>
 
       <Footer class=" absolute left-[calc(50%-80px)] bottom-0" />
@@ -37,6 +27,9 @@ import PathAnimatePanel from './PathAnimatePanel.vue'
 
 <style scoped>
 .app-wrapper{
+  position: fixed;
+  top: 0;
+  left: 0;
   background-image: url("http://zhongguose.com/img/texture.png")
 }
 </style>
