5d895f7fdd4489939f53d1e33d8f4f5d115ebbf0
pinky-pig <2545589993@qq.com>
2023-06-28 16:32:49 +0800

    fix: sidebar styles

--- src/components/ui/CCollapse.vue
+++ src/components/ui/CCollapse.vue
@@ -5,12 +5,14 @@ const show = ref(true)
 <template>
   <div class=" flex flex-col flex-auto overflow-auto text-start">
     <div
-      class=" flex flex-row tracking-widest cursor-pointer my-10px select-none"
+      class=" flex flex-row tracking-widest cursor-pointer my-10px select-none justify-between items-center"
       @click="show = !show"
     >
       <slot name="title" />
+      <div v-if="show" class="opacity-70" i-carbon:chevron-up />
+      <div v-else class="opacity-70" i-carbon:chevron-down />
     </div>
-
+    <hr v-if="!show" class="opacity-10">
     <div class=" my-5px ">
       <n-collapse-transition :show="show">
         <slot name="default" />
